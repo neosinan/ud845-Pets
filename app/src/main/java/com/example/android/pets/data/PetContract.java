99ec976e2a2f93b741fb09a2ef8bcd08873ed214
csprneo <sinan50563@gmail.com>
2017-10-30 17:15:07 +0300

    Content provider added as empty

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -26,11 +27,22 @@ public final class PetContract {
     // give it an empty constructor.
     private PetContract() {}
 
+    //Our authority
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_PETS = "pets";
+
     /**
      * Inner class that defines constant values for the pets database table.
      * Each entry in the table represents a single pet.
      */
     public static final class PetEntry implements BaseColumns {
+
+
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         /** Name of database table for pets */
         public final static String TABLE_NAME = "pets";
